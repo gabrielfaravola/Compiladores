786654818540d72e46f8a929b81dcdd9064cb9e1
gabrielfaravola <gabrielfaravola@gmail.com>
2026-04-16 13:15:00 -0300

    Formatando testes

--- Entrega1/exemplo1.py
+++ Entrega1/exemplo1.py
@@ -1,6 +1,6 @@
-vetorList = [2, 3, 5, 7, 11]
+vetorList = [ 2 , 3 , 5 , 7 , 11 ]
 
 i = 0
-while i < len(vetorList):
-    print(vetorList[i])
+while i < len ( vetorList ) :
+    print ( vetorList [ i ] )
     i = i + 1
--- Entrega1/exemplo2.py
+++ Entrega1/exemplo2.py
@@ -1,8 +1,8 @@
-number1 = input("digite um numero: ")
-number2 = input("digite outro numero: ")
+number1 = input ( "digite um numero: " )
+number2 = input ( "digite outro numero: " )
 if number1 > number2:
-    print "o maior numero eh ---> ", number1
+    print "o maior numero eh ---> " , number1
 if number2 > number1:
-    print "o maior numero eh ---> ", number2
-else:
-    print number1, " = ", number2
+    print "o maior numero eh ---> " , number2
+else :
+    print number1 , " = " , number2
